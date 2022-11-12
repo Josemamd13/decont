1b116f475680eef4db1f7bf860ed47c21fdabfa0
José Manuel Muñoz Díaz <josemadb13898@gmail.com>
2022-11-12 13:10:29 +0100

    STAR index

--- scripts/index.sh
+++ scripts/index.sh
@@ -1,3 +1,17 @@
+echo "Running STAR index..."
+
+mkdir -p res/contaminants_idx
+
+	STAR \
+		--runThreadN 8 \
+		--runMode genomeGenerate \
+		--genomeDir $2 \
+		--genomeFastaFiles $1 \
+		--genomeSAindexNbases 9
+
+echo
+
+
 # This script should index the genome file specified in the first argument ($1),
 # creating the index in a directory specified by the second argument ($2).
 
