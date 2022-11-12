bc6eb02a8c7f63f136bfd802a1eb4b38a49323bc
José Manuel Muñoz Díaz <josemadb13898@gmail.com>
2022-11-12 12:17:19 +0100

    First attempt for download.sh script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,3 +1,11 @@
+wget -P $2 $1
+
+if [ "$3" = "yes" ]
+then
+	gunzip -k $2/$(basename $1)
+fi
+
+
 # This script should download the file specified in the first argument ($1),
 # place it in the directory specified in the second argument ($2),
 # and *optionally*:
