b4944f334bfee2228c7db926f76daa50fbcb7dda
José Manuel Muñoz Díaz <josemadb13898@gmail.com>
2022-12-15 12:17:22 +0100

    Replacement of the deleting.sh script.

---
+++ scripts/cleanup.sh
@@ -0,0 +1,25 @@
+###CLEANUP SCRIPT BY José Manuel Muñoz###
+
+#Data
+cd data
+rm *.gz
+cd ..
+
+#Envs
+cd envs
+rm -r *
+cd ..
+
+#Log
+cd log
+rm -r *
+cd ..
+
+#Out
+cd out
+rm -r *
+cd ..
+
+#Res
+cd res
+rm -r *
