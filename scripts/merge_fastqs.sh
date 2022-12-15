e4da009887e77b5460c60cbd579ca95984b8b77c
José Manuel Muñoz Díaz <josemadb13898@gmail.com>
2022-12-15 12:16:12 +0100

    Minor changes.

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,12 +1,12 @@
+###MERGE SCRIPT BY JOSE MANUEL MUÑOZ###
+
 # This script should merge all files from a given sample (the sample id is
 # provided in the third argument ($3)) into a single file, which should be
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
-echo "Merging compressed files..."
 
 mkdir -p out/merged
 
 	cat $1/$3* > $2/$3.fastq.gz
 
-echo
