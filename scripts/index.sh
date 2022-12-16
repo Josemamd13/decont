9eababda17a447ca121921c40f6e02803994cac5
José Manuel Muñoz Díaz <josemadb13898@gmail.com>
2022-12-16 11:50:11 +0100

    Script variables defined.

--- scripts/index.sh
+++ scripts/index.sh
@@ -9,6 +9,8 @@
 # STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
 # --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
 
+outdir=$2
+genomefile=$1
 
 mkdir -p res/contaminants_idx
 
