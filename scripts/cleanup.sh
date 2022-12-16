ea01d74ead931ae0ae92fdda5b9d5f827496772e
José Manuel Muñoz Díaz <josemadb13898@gmail.com>
2022-12-16 11:53:01 +0100

    Complete change of the script, removal of selective directories.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,25 +1,39 @@
 ###CLEANUP SCRIPT BY José Manuel Muñoz###
 
-#Data
-cd data
-rm *.gz
-cd ..
+if [ "$#" -eq 0 ]
+then
+	find data ~/decont -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep -o -name .gitignore -o -name README.md \) -delete
+	find log envs out res -mindepth 1 ! -name .gitkeep -delete
+	
+	echo -e "\t\t##### Everything has been deleted. #####"
+else
+	args=$@
+fi
 
-#Envs
-cd envs
-rm -r *
-cd ..
 
-#Log
-cd log
-rm -r *
-cd ..
-
-#Out
-cd out
-rm -r *
-cd ..
-
-#Res
-cd res
-rm -r *
+for arg in $args
+do
+	case $arg in
+		"data")
+			find data -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep \) -delete
+			echo "$arg directory is empty"
+			;;
+		"log")
+			find log -mindepth 1 ! -name .gitkeep -delete
+			echo "$arg directory is empty"
+			;;
+		"out")
+                        find out -mindepth 1 ! -name .gitkeep -delete
+                        echo "$arg directory is empty"
+			;;
+		"res")
+			find res -mindepth 1 ! -name .gitkeep -delete
+                        echo "$arg directory is empty"
+			;;
+		"envs")
+			find envs -mindepth 1 ! -name .gitkeep -delete
+			echo "$arg directory is empty"
+			;;
+		*) echo "$arg directory does not exist..."
+		esac
+done
