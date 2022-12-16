adb40e11b28efb083cc5e9d50e65c80c29b0d5ca
José Manuel Muñoz Díaz <josemadb13898@gmail.com>
2022-12-16 12:21:38 +0100

    Improved code readability.

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,10 +1,10 @@
 ###RNA DECONTAMINATION PIPELINE BY José Manuel Muñoz###
 
-echo -e "\n\n### Pipeline started at $(date +'%H:%M:%S') ###\n\n"
+echo -e "\n\n~~~ Pipeline started at $(date +'%H:%M:%S')... ~~~\n\n"
 
 #Run cleanup script at the beginning to ensure that data is not duplicated
 
-#bash scripts/cleanup.sh 2>> log/errors.log
+bash scripts/cleanup.sh 2>> log/errors.log
 
 #Download all the files specified in data/filenames
 #for url in $(<list_of_urls>) #TODO
@@ -12,33 +12,33 @@ echo -e "\n\n### Pipeline started at $(date +'%H:%M:%S') ###\n\n"
 #    bash scripts/download.sh $url data
 #done
 
-echo -e "\n##### Downloading files... #####\n"
+echo -e "\n~~~~~ Downloading files... ~~~~~\n"
 
 for url in $(cat data/urls)
 do
 	bash scripts/download.sh $url data
 done
 
-echo -e "\n\t\t###### Done. #####\n"
+echo -e "\n\t\t\t~~~~~ Done. ~~~~~\n"
 
 # Download the contaminants fasta file, uncompress it, and
 # filter to remove all small nuclear RNAs
 #bash scripts/download.sh <contaminants_url> res yes #TODO
 
-echo -e "\n##### Uncompressing files... #####\n"
+echo -e "\n~~~~~ Uncompressing files... ~~~~~\n"
 
 bash scripts/download.sh https://bioinformatics.cnio.es/data/courses/decont/contaminants.fasta.gz res yes filter
 
-echo -e "\n\t\t##### Done. #####\n"
+echo -e "\n\t\t\t~~~~~ Done. ~~~~~\n"
 
 # Index the contaminants file
 #bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
-echo -e "\n##### Running STAR index... #####\n"
+echo -e "\n~~~~~ Running STAR index... ~~~~~\n"
 
 bash scripts/index.sh res/contaminants.fasta res/contaminants_idx
 
-echo -e "\n\t\t##### Done. #####\n"
+echo -e "\n\t\t\t~~~~~ Done. ~~~~~\n"
 
 # Merge the samples into a single file
 #for sid in $(<list_of_sample_ids>) #TODO
@@ -46,21 +46,21 @@ echo -e "\n\t\t##### Done. #####\n"
 #    bash scripts/merge_fastqs.sh data out/merged $sid
 #done
 
-echo -e "\n##### Merging compressed files... #####\n"
+echo -e "\n~~~~~ Merging compressed files... ~~~~~\n"
 
 for sid in $(ls data/*.fastq.gz | cut -d "-" -f1 | sed 's:data/::' | sort | uniq)
 do
-	echo -e "Merging $sid sample..."
+	echo -e "Merging $sid sample... \n"
 	bash scripts/merge_fastqs.sh data out/merged $sid
 done
 
-echo -e "\n\t\t##### Done. #####\n"
+echo -e "\n\t\t\t~~~~~ Done. ~~~~~\n"
 
 # TODO: run cutadapt for all merged files
 # cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
 #     -o <trimmed_file> <input_file> > <log_file>
 
-echo -e "\n##### Running cutadapt... #####\n"
+echo -e "\n~~~~~ Running cutadapt... ~~~~~\n"
 
 mkdir -p out/trimmed
 mkdir -p log/cutadapt
@@ -74,7 +74,7 @@ do
                 -o out/trimmed/${sid}.trimmed.fastq.gz out/merged/${sid}.fastq.gz > log/cutadapt/${sid}.log
 done
 
-echo -e "\n\t\t##### Done. #####\n"
+echo -e "\n\t\t\t~~~~~ Done. ~~~~~\n"
 
 # TODO: run STAR for all trimmed files
 #for fname in out/trimmed/*.fastq.gz
@@ -87,7 +87,7 @@ echo -e "\n\t\t##### Done. #####\n"
     #    --readFilesCommand gunzip -c --outFileNamePrefix <output_directory>
 #done
 
-echo -e "\n##### Running STAR... #####\n"
+echo -e "\n~~~~~ Running STAR... ~~~~~\n"
 
 for fname in out/trimmed/*.fastq.gz
 do
@@ -105,7 +105,7 @@ mkdir -p out/star/$sid
                 --outFileNamePrefix out/star/${sid}/
 done
 
-echo -e "\n\t\t##### Done. #####\n"
+echo -e "\n\t\t\t~~~~~ Done. ~~~~~\n"
 
 # TODO: create a log file containing information from cutadapt and star logs
 # (this should be a single log file, and information should be *appended* to it on each run)
@@ -113,7 +113,7 @@ echo -e "\n\t\t##### Done. #####\n"
 # - star: Percentages of uniquely mapped reads, reads mapped to multiple loci, and to too many loci
 # tip: use grep to filter the lines you're interested in
 
-echo -e "\n##### Creating a log file containing information from cutadapt and STAR logs... #####\n"
+echo -e "\n~~~~~ Creating a log file containing information from cutadapt and STAR logs... ~~~~~\n"
 
 for sid in $(ls data/*.fastq.gz | cut -d "-" -f1 | sed 's:data/::' | sort | uniq)
 do
@@ -133,14 +133,14 @@ do
 	echo -e "\n" >>log/pipeline.log
 done
 
-echo -e "\n\t\t##### Log file created. #####\n"
+echo -e "\n\t\t\t~~~~~ Log file created. ~~~~~\n"
 
-echo -e "\n##### Saving the environment... #####\n"
+echo -e "\n~~~~~ Saving the environment... ~~~~~\n"
 
 mkdir -p envs
 
 conda env export > envs/decont.yaml
 
-echo -e "\n\t\t##### Environment saved. #####\n"
+echo -e "\n\t\t\t~~~~~ Environment saved. ~~~~~\n"
 
-echo -e "\n\n### Pipeline finished at $(date +'%H:%M:%S') ###\n\n"
+echo -e "\n\n~~~ Pipeline finished at $(date +'%H:%M:%S') ~~~\n\n"
