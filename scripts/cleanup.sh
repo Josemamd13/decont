da01a830a6684aa0678cd1ab23e26d6f7856c637
José Manuel Muñoz Díaz <josemadb13898@gmail.com>
2022-12-19 12:24:05 +0100

    Explanatory comments added.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,8 +1,10 @@
 ###CLEANUP SCRIPT BY José Manuel Muñoz###
 
+#If we do not pass any argument to the script, what it will do is to delete all 
+#the files that it finds with a depth of 1, except for those that we protect (!)
 if [ "$#" -eq 0 ]
 then
-	find data ~/decont -maxdepth 1 -type f ! \( -name urls -o -name .gitkeep -o -name .gitignore -o -name README.md \) -delete
+	find data ~/decont -maxdepth 1 -type f ! ( -name urls -o -name .gitkeep -o -name .gitignore -o -name README.md \) -delete
 	find log envs out res -mindepth 1 ! -name .gitkeep -delete
 	
 	echo -e "\t\t##### Everything has been deleted. #####"
@@ -10,7 +12,8 @@ else
 	args=$@
 fi
 
-
+#In case we pass any argument ("data", "log", "out", "res" or "envs") only files
+#belonging to that directory with a depth of 1 will be deleted.
 for arg in $args
 do
 	case $arg in
